512d94589ed6070ece0fc2bae3c6d6a50be6c22d
Aravind <145030874+Aravindkammari@users.noreply.github.com>
2025-05-27 13:45:32 +0530

    Time: 4 ms (74.73%), Space: 45.3 MB (19.14%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(word1, word2, m - 1, n - 1, dp);
+    }
+    int helper(String word1, String word2, int i, int j, int[][] dp){
+        if(i < 0)return j + 1;
+        if(j < 0)return i + 1;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(word1.charAt(i) == word2.charAt(j))return helper(word1, word2, i - 1, j - 1, dp);
+        else{
+            int insert = helper(word1, word2, i, j - 1, dp);
+            int delete = helper(word1, word2, i - 1, j, dp);
+            int replace = helper(word1, word2, i - 1, j - 1, dp);
+            dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+        }
+        return dp[i][j];
+    }
+}

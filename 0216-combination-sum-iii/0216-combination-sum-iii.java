394988e7bdcdb1890101cc59ecde2ff26c74b009
Aravind <145030874+Aravindkammari@users.noreply.github.com>
2025-07-10 00:25:18 +0530

    Time: 0 ms (100%), Space: 41.3 MB (27.95%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(ans, new ArrayList(){}, 1, k, n);
-        return ans;
+        List<List<Integer>> mainList = new ArrayList<>();
+        helper(mainList, new ArrayList<>(), k, n, 1);
+        return mainList;
     }
-    void helper(List<List<Integer>> ans, List<Integer> list, int i, int k, int n){
-        if(n == 0 && k == 0){
-            ans.add(new ArrayList<>(list));
+    void helper(List<List<Integer>> mainList, List<Integer> list, int k, int n, int i){
+        if(n < 0)return;
+        if(list.size() == k && n == 0){
+            mainList.add(new ArrayList<>(list));
             return;
         }
-        if(i > 9)return;
-        if(n < 0 || k < 0)return;
+        if(i > 9 || k < list.size())return;
         list.add(i);
-        helper(ans, list, i+1, k-1, n-i);
-        list.remove(list.size()-1);
-        helper(ans, list, i+1, k, n);
+        helper(mainList, list, k, n - i, i + 1);
+        list.remove(list.size() - 1);
+        helper(mainList, list, k, n, i + 1);
     }
 }

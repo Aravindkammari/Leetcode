a2be1c0ea0a90fc5b4b46dda3a7ab11567c179f2
Aravind <145030874+Aravindkammari@users.noreply.github.com>
2025-08-22 19:40:10 +0530

    Time: 2 ms (66.78%), Space: 43.6 MB (5.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -2,34 +2,38 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
-        int fresh = 0, time = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1)fresh++;
-                else if(grid[i][j] == 2) q.offer(new int[] {i, j});
+                if(grid[i][j] == 1){
+                    fresh++;
+                }else if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j});
+                }
             }
         }
+        int time = 0;
         if(fresh == 0)return 0;
         while(!q.isEmpty()){
-            int s = q.size();
-            for(int i = 0; i < s; i++){
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
+            int k = q.size();
+            for(int i = 0; i < k; i++){
+                int[] arr = q.poll();
+                int x = arr[0];
+                int y = arr[1];
                 int[][] neighbours = {{x-1, y}, {x, y-1}, {x+1, y}, {x, y+1}};
                 for(int[] neighbour : neighbours){
                     int nr = neighbour[0];
                     int nc = neighbour[1];
-                    if(nr < 0 || nr >= m || nc < 0 || nc >= n || grid[nr][nc] != 1)continue;
-                    q.offer(new int[] {nr, nc});
+                    if(nr < 0 || nc < 0 || nr >= m || nc >= n || grid[nr][nc] != 1)continue;
                     grid[nr][nc] = 2;
+                    q.offer(new int[] {nr, nc});
+                    fresh--;
                 }
             }
-            fresh -= q.size();
             time++;
             if(fresh == 0)break;
         }
-        return fresh == 0 ? time : -1;
+        return (fresh == 0) ? time : -1;
     }
 }
